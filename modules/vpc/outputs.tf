fd7314db028f025a0afc518649e6ec196c499bc3
chaninduimanjith <chanindu.imanjith@gmail.com>
2026-09-11 21:42:47 +0530

    Export VPC, subnet IDs, and NAT gateway public IPs

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value       = aws_vpc.this.id
+  description = "The ID of the VPC"
+}
+
+output "public_subnet_ids" {
+  value       = aws_subnet.public[*].id
+  description = "List of IDs of public subnets"
+}
+
+output "private_subnet_ids" {
+  value       = aws_subnet.private[*].id
+  description = "List of IDs of private subnets"
+}
+
+output "nat_gateway_ips" {
+  value       = aws_eip.nat[*].public_ip
+  description = "Public Elastic IP addresses of NAT Gateways"
+}
