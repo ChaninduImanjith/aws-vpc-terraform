6796e9de316e0d4520c4df0acce6e3f2073132a7
chaninduimanjith <chanindu.imanjith@gmail.com>
2026-09-12 15:33:49 +0530

    Fix terraform code formatting

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_cidr                 = var.vpc_cidr
-  public_subnet_cidrs      = var.public_subnet_cidrs
-  private_subnet_cidrs     = var.private_subnet_cidrs
+  vpc_cidr                  = var.vpc_cidr
+  public_subnet_cidrs       = var.public_subnet_cidrs
+  private_subnet_cidrs      = var.private_subnet_cidrs
   enable_single_nat_gateway = var.enable_single_nat_gateway
 }
 
